2f0f6db9767503b8a56a342b6c4f937b27627f72
Chris Hamons <chris.hamons@xamarin.com>
2019-02-04 13:50:01 -0600

    Add explain script

---
+++ tools/explain.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+~/bin/clio/clio --github=xamarin/xamarin-macios --github-pat-file=/Users/donblas/.github-general-pat --explain-commit=$1 /Users/donblas/Programming/macios/master/xamarin-macios
